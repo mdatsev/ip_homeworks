88a2088a2e255b2cc0dee9af5d28cbf35d4ea923
mdatsev <martin.datsev.p@gmail.com>
2018-09-27 23:13:55 +0300

    Implemented chat

--- EchoServer/src/org/elsys/netprog/sockets/EchoServer.java
+++ EchoServer/src/org/elsys/netprog/sockets/EchoServer.java
@@ -6,6 +6,46 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+
+class ChatHandler extends Thread {
+	private ChatSocket socket;
+	private ArrayList<ChatSocket> sockets;
+	
+	public ChatHandler(ChatSocket socket, ArrayList<ChatSocket> sockets) throws IOException {
+		this.socket = socket;
+		this.sockets = sockets;
+	}
+	
+	@Override
+	public void run() {
+	    String userInput;
+	    try {
+			while ((userInput = socket.in.readLine()) != null)
+			{
+				for(ChatSocket s : sockets) {
+					if(s != socket)
+						s.out.println(userInput);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class ChatSocket {
+	public Socket socket;
+	public PrintWriter out;
+	public BufferedReader in;
+	
+	public ChatSocket(Socket socket) throws IOException {
+		this.socket = socket;
+	    out = new PrintWriter(socket.getOutputStream(), true);
+	    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	}
+	
+}
 
 public class EchoServer {
 
@@ -13,20 +53,14 @@ public class EchoServer {
 		ServerSocket serverSocket = null;
 		try {
 			serverSocket = new ServerSocket(10001);
-		    Socket clientSocket = serverSocket.accept();
-		    System.out.println("client connected from " + clientSocket.getInetAddress());
-		    PrintWriter out =
-		        new PrintWriter(clientSocket.getOutputStream(), true);
-		    BufferedReader in = new BufferedReader(
-		        new InputStreamReader(clientSocket.getInputStream()));
-		    
-		    String inputLine;
-
-		    while ((inputLine = in.readLine()) != null) {
-		        out.println(inputLine);
-		        System.out.println(inputLine);
-		        if (inputLine.equals("exit"))
-		            break;
+		    ArrayList<ChatSocket> clientSockets = new ArrayList<>();
+		    for(;;)
+		    {
+		    	ChatSocket clientSocket = new ChatSocket(serverSocket.accept());
+		    	clientSockets.add(clientSocket);
+		    	System.out.println("client connected");
+		    	ChatHandler handler = new ChatHandler(clientSocket, clientSockets);
+		    	handler.start();
 		    }
 		} catch (Throwable t) {
 			System.out.println(t.getMessage());
--- SocketClient/src/org/elsys/netprog/sockets/SocketsClient.java
+++ SocketClient/src/org/elsys/netprog/sockets/SocketsClient.java
@@ -4,26 +4,53 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
 import java.net.InetAddress;
 import java.net.Socket;
+
+class ReaderWriter extends Thread {
+
+	private BufferedReader in;
+	private PrintWriter out;
+	
+	ReaderWriter(BufferedReader in, PrintWriter out) {
+		this.in = in;
+		this.out = out;
+	}
+	
+	@Override
+	public void run() {
+	    String userInput;
+	    try {
+			while ((userInput = in.readLine()) != null)
+			{
+			    out.println(userInput);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
 
 public class SocketsClient {
 
 	public static void main(String[] args) throws IOException {
 		Socket echoSocket = null;
 		try {
-			    echoSocket = new Socket("localhost", 10001);
-			    PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-			    BufferedReader in = new BufferedReader(
-			            new InputStreamReader(echoSocket.getInputStream()));
-			    BufferedReader stdIn = new BufferedReader(
-			            new InputStreamReader(System.in));
-			    
-			    String userInput;
-			    while ((userInput = stdIn.readLine()) != null) {
-			        out.println(userInput);
-			        System.out.println("server response: " + in.readLine());
-			    }
+		    echoSocket = new Socket("localhost", 10001);
+		    PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+		    BufferedReader in = new BufferedReader(
+		            new InputStreamReader(echoSocket.getInputStream()));
+		    BufferedReader stdIn = new BufferedReader(
+		            new InputStreamReader(System.in));
+		    
+		    ReaderWriter sender = new ReaderWriter(stdIn, out);
+		    sender.start();
+		    ReaderWriter reciever = new ReaderWriter(in, new PrintWriter(System.out, true));
+		    reciever.start();
+		    sender.join();
+		    reciever.join();
 		} catch (Throwable t) {
 			System.out.println(t.getMessage());
 		} finally {
