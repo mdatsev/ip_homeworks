981c530aae16eb309f711d0f65dde7f6e90da201
mdatsev <martin.datsev.p@gmail.com>
2018-12-09 13:13:48 +0200

    fixes :/

--- 4/input.js
+++ 4/input.js
@@ -3,8 +3,8 @@ regExps = {
 "exercise_2": /088[^0]\d{6}/,
 "exercise_3": /[^01]+/,
 "exercise_4": /^[a-z][\w.]{2,30}$/,
-"exercise_5": /999|1[0-4]\d+|1500/,
-"exercise_6": /\w+=.+(?= )/
+"exercise_5": /999|1[0-4]\d{2}|1500/,
+"exercise_6": /class=.+(?= )/
 };
 cssSelectors = {
 "exercise_1": "item>java",
